64da1d498edfc5291f10f7e972ab76522a927bc5
Santamanches <santamanches@gmail.com>
2023-03-01 20:34:22 +0300

    save

---
+++ app.js
@@ -0,0 +1,15 @@
+const app = {
+    
+    data(){
+        return {
+            value:'',
+            background: '',
+            color1: 'color1',
+            color2: 'color2',
+            color3: 'color3',
+        }
+    },
+}
+
+
+Vue.createApp(app).mount('#app')
